dcd0f41e804183c63d24536537c945c81b5fd5ed
nicolassack5 <nicolassack2002@gmail.com>
2024-06-30 22:48:53 -0300

    cambios un millon

--- EliminarServicio.js
+++ EliminarServicio.js
@@ -1,165 +1,116 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TouchableOpacity, Image, StyleSheet, TextInput, Button, ScrollView } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
+import { View, Text, TouchableOpacity, ScrollView, StyleSheet, Image, StatusBar } from 'react-native';
 import axios from 'axios';
-
-// Componente para mostrar los detalles de un servicio en una tarjeta
-const ServiceCard = ({ servicio, onPress }) => (
-  <TouchableOpacity style={styles.serviceItem} onPress={onPress}>
-    <Text>{servicio.id}</Text>
-    {/* Agrega aquí otros detalles del servicio si es necesario */}
-  </TouchableOpacity>
-);
+import { Ionicons } from '@expo/vector-icons';
 
 const EliminarServicio = ({ route, navigation }) => {
-  const [idServicioComercio, setIdServicioComercio] = useState('');
-  const [idServicioProfesional, setIdServicioProfesional] = useState('');
-  const [serviciosComercio, setServiciosComercio] = useState([]);
+  const [mail] = useState(route.params.mail);
   const [serviciosProfesionales, setServiciosProfesionales] = useState([]);
-  const [mostrarFormularioComercio, setMostrarFormularioComercio] = useState(true); // Mostrar el formulario de comercio por defecto
-  const [mostrarFormularioProfesional, setMostrarFormularioProfesional] = useState(false);
+  const [serviciosComercio, setServiciosComercio] = useState([]);
+  const [mostrarProfesionales, setMostrarProfesionales] = useState(true); // Estado para mostrar servicios profesionales
+  const [mostrarComercio, setMostrarComercio] = useState(false); // Estado para mostrar servicios de comercio
 
-  const { mail } = route.params || {};
-
-  // Obtener los IDs de los servicios disponibles al cargar el componente
-  useEffect(() => {
-    fetchServiciosComercio();
-    fetchServiciosProfesionales();
-  }, []);
-
-  // Función para obtener los servicios de comercio
-  const fetchServiciosComercio = async () => {
-    try {
-      const response = await axios.get(`http://192.168.0.241:8080/inicio/misServiciosComercio?mail=${mail}`);
-      setServiciosComercio(response.data);
-    } catch (error) {
-      console.error('Error al obtener los servicios de comercio:', error);
-      // Manejar errores aquí
-    }
-  };
-
-  // Función para obtener los servicios profesionales
+  // Obtener los servicios profesionales
   const fetchServiciosProfesionales = async () => {
     try {
-      const response = await axios.get(`http://192.168.0.241:8080/inicio/misServiciosProfesionales?mail=${mail}`);
+      const response = await axios.get(`http://localhost:8080/inicio/misServiciosProfesionales?mail=${mail}`);
       setServiciosProfesionales(response.data);
     } catch (error) {
       console.error('Error al obtener los servicios profesionales:', error);
-      // Manejar errores aquí
     }
   };
 
-  // Función para eliminar un servicio de comercio por su ID
-  const eliminarServicioComercio = async () => {
+  // Obtener los servicios de comercio
+  const fetchServiciosComercio = async () => {
     try {
-      const url = `http://192.168.0.241:8080/inicio/eliminarServicioComercio?mail=${mail}&idServicio=${idServicioComercio}`;
-      const response = await axios.delete(url);
-      console.log('Respuesta de eliminación de servicio de comercio:', response.data);
-      // Manejar la respuesta del servidor según sea necesario
+      const response = await axios.get(`http://localhost:8080/inicio/misServiciosComercio?mail=${mail}`);
+      setServiciosComercio(response.data);
     } catch (error) {
-      console.error('Error al eliminar el servicio de comercio:', error);
-      // Manejar errores aquí
+      console.error('Error al obtener los servicios de comercio:', error);
     }
   };
 
-  // Función para eliminar un servicio profesional por su ID
-  const eliminarServicioProfesional = async () => {
-    try {
-      const url = `http://192.168.0.241:8080/inicio/eliminarServicioProfesional?mail=${mail}&idServicio=${idServicioProfesional}`;
-      const response = await axios.delete(url);
-      console.log('Respuesta de eliminación de servicio profesional:', response.data);
-      // Manejar la respuesta del servidor según sea necesario
-    } catch (error) {
-      console.error('Error al eliminar el servicio profesional:', error);
-      // Manejar errores aquí
-    }
+  useEffect(() => {
+    fetchServiciosProfesionales();
+    fetchServiciosComercio();
+  }, []);
+
+  // Función para cambiar entre la sección de servicios profesionales y servicios de comercio
+  const mostrarServiciosProfesionales = () => {
+    setMostrarProfesionales(true);
+    setMostrarComercio(false);
+  };
+
+  const mostrarServiciosComercio = () => {
+    setMostrarProfesionales(false);
+    setMostrarComercio(true);
   };
 
   return (
     <View style={styles.container}>
-      {/* Encabezado y botones de navegación */}
+      <StatusBar />
       <View style={styles.header}>
         <TouchableOpacity style={styles.backIcon} onPress={() => navigation.goBack()}>
           <Ionicons name="arrow-back" size={24} color="white" />
         </TouchableOpacity>
-        <Text style={styles.headerTitle}>Mimuni</Text>
+        <Text style={styles.headerTitle}>MiMuni</Text>
         <View style={{ width: 20 }}></View>
       </View>
 
-      {/* Título de Eliminar Servicio */}
       <Text style={styles.title}>Eliminar Servicio</Text>
 
-      {/* Botones para seleccionar tipo de formulario */}
+      {/* Botones para cambiar entre las secciones */}
       <View style={styles.toggleButtons}>
-        <TouchableOpacity style={[styles.toggleButton, mostrarFormularioComercio && styles.activeButton]} onPress={() => {setMostrarFormularioComercio(true); setMostrarFormularioProfesional(false); fetchServiciosComercio();}}>
-          <Text style={mostrarFormularioComercio ? styles.activeButtonText : styles.buttonText}>Servicio Comercio</Text>
+        <TouchableOpacity style={[styles.toggleButton, mostrarProfesionales && styles.activeButton]} onPress={mostrarServiciosProfesionales}>
+          <Text style={mostrarProfesionales ? styles.activeButtonText : styles.buttonText}>Servicios Profesionales</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={[styles.toggleButton, mostrarFormularioProfesional && styles.activeButton]} onPress={() => {setMostrarFormularioProfesional(true); setMostrarFormularioComercio(false); fetchServiciosProfesionales();}}>
-          <Text style={mostrarFormularioProfesional ? styles.activeButtonText : styles.buttonText}>Servicio Profesional</Text>
+        <TouchableOpacity style={[styles.toggleButton, mostrarComercio && styles.activeButton]} onPress={mostrarServiciosComercio}>
+          <Text style={mostrarComercio ? styles.activeButtonText : styles.buttonText}>Servicios de Comercio</Text>
         </TouchableOpacity>
       </View>
 
-      {/* Formulario de servicio seleccionado */}
-      <ScrollView contentContainerStyle={styles.scrollContent}>
-        {mostrarFormularioComercio && (
-          <View style={styles.formContainer}>
-            <Text style={styles.subtitle}>Servicios de Comercio Disponibles:</Text>
-            <ScrollView style={styles.scrollView} contentContainerStyle={styles.serviceList}>
-              {serviciosComercio.map(servicio => (
-                <ServiceCard
-                  key={servicio.id}
-                  servicio={servicio}
-                  onPress={() => setIdServicioComercio(servicio.id)}
-                />
-              ))}
-            </ScrollView>
-            <TextInput
-              style={styles.input}
-              placeholder="Ingrese el ID del servicio de comercio a eliminar"
-              value={idServicioComercio}
-              onChangeText={setIdServicioComercio}
-            />
-            <Button title="Eliminar Servicio de Comercio" onPress={eliminarServicioComercio} />
+      {/* Mostrar servicios */}
+      <ScrollView style={styles.scrollView}>
+        {mostrarProfesionales && serviciosProfesionales.map(servicio => (
+          <View key={servicio.idservicioprofesional} style={styles.servicioContainer}>
+            <Text>ID: {servicio.idservicioprofesional}</Text>
+            <Text>Nombre: {servicio.nombre} {servicio.apellido}</Text>
+            <Text>Contacto: {servicio.contacto}</Text>
+            <Text>Horario: {servicio.horario}</Text>
+            <Text>Rubro: {servicio.rubro}</Text>
+            <Text>Descripción: {servicio.descripcion}</Text>
+            <Text>Estado: {servicio.estado}</Text>
           </View>
-        )}
+        ))}
 
-        {mostrarFormularioProfesional && (
-          <View style={styles.formContainer}>
-            <Text style={styles.subtitle}>Servicios Profesionales Disponibles:</Text>
-            <ScrollView style={styles.scrollView} contentContainerStyle={styles.serviceList}>
-              {serviciosProfesionales.map(servicio => (
-                <ServiceCard
-                  key={servicio.id}
-                  servicio={servicio}
-                  onPress={() => setIdServicioProfesional(servicio.id)}
-                />
-              ))}
-            </ScrollView>
-            <TextInput
-              style={styles.input}
-              placeholder="Ingrese el ID del servicio profesional a eliminar"
-              value={idServicioProfesional}
-              onChangeText={setIdServicioProfesional}
-            />
-            <Button title="Eliminar Servicio Profesional" onPress={eliminarServicioProfesional} />
+        {mostrarComercio && serviciosComercio.map(servicio => (
+          <View key={servicio.idServicioComercio} style={styles.servicioContainer}>
+            <Text>ID: {servicio.idServicioComercio}</Text>
+            <Text>Dirección: {servicio.direccion}</Text>
+            <Text>Contacto: {servicio.contacto}</Text>
+            <Text>Descripción: {servicio.descripcion}</Text>
+            <Text>Estado: {servicio.estado}</Text>
           </View>
-        )}
+        ))}
       </ScrollView>
 
-      {/* Navegación inferior */}
+      {/* Navbar */}
       <View style={styles.navbar}>
         <TouchableOpacity style={styles.navButton} onPress={() => navigation.navigate('ServiciosVecino', { mail })}>
           <Image source={require('./assets/servicios.png')} style={styles.icon} />
           <Text style={styles.navText}>Servicios</Text>
         </TouchableOpacity>
+
         <TouchableOpacity style={styles.navButton} onPress={() => navigation.navigate('ReclamosVecino', { mail })}>
           <Image source={require('./assets/reclamos.png')} style={styles.icon} />
           <Text style={styles.navText}>Reclamos</Text>
         </TouchableOpacity>
+
         <TouchableOpacity style={styles.navButton}>
           <Image source={require('./assets/denuncias.png')} style={styles.icon} />
           <Text style={styles.navText}>Denuncias</Text>
         </TouchableOpacity>
+
         <TouchableOpacity style={styles.navButton} onPress={() => navigation.navigate('PerfilVecino', { mail })}>
           <Image source={require('./assets/perfil.png')} style={styles.icon} />
           <Text style={styles.navText}>Perfil</Text>
@@ -173,6 +124,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#F2E9E4',
+    paddingTop: 0,
   },
   header: {
     backgroundColor: '#4A4E69',
@@ -181,91 +133,63 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-  },
-  headerTitle: {
-    color: '#FFFFFF',
-    fontSize: 30,
-    fontWeight: 'bold',
-    textAlign: 'center',
+    height: 130,
   },
   backIcon: {
-    justifyContent: 'center',
-    alignItems: 'center',
+    padding: 5,
+  },
+  headerTitle: {
+    color: 'white',
+    fontSize: 24,
+    fontWeight: 'bold',
   },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
     textAlign: 'center',
-    marginVertical: 10,
+    marginBottom: 20,
   },
   toggleButtons: {
     flexDirection: 'row',
-    justifyContent: 'space-around',
-    marginVertical: 10,
-  },
-  toggleButton: {
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    backgroundColor: '#4A4E69',
-    borderRadius: 20,
-  },
-  activeButton: {
-    backgroundColor: '#6D6D6D',
-  },
-  buttonText: {
-    color: '#FFFFFF',
-    fontSize: 16,
-  },
-  activeButtonText: {
-    color: '#FFFFFF',
-    fontSize: 16,
-  },
-  scrollContent: {
-    flexGrow: 1,
-    justifyContent: 'space-between',
-  },
-  formContainer: {
     justifyContent: 'center',
-    alignItems: 'center',
-    paddingHorizontal: 20,
-    marginBottom: 100,
-  },
-  subtitle: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginBottom: 10,
-    textAlign: 'center',
-  },
-  scrollView: {
-    maxHeight: 200,
     marginBottom: 20,
   },
-  serviceList: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'center',
-  },
-  serviceItem: {
-    backgroundColor: '#DDD',
-    padding: 10,
-    margin: 5,
+  toggleButton: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
     borderRadius: 5,
+    backgroundColor: '#ccc',
+    marginHorizontal: 10,
   },
-  input: {
-    height: 40,
-    width: '100%',
-    borderColor: '#DDD',
-    borderWidth: 1,
-    borderRadius: 5,
-    paddingHorizontal: 10,
+  activeButton: {
+    backgroundColor: '#007bff',
+  },
+  buttonText: {
+    color: '#333',
+    fontWeight: 'bold',
+  },
+  activeButtonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+  scrollView: {
+    flex: 1,
+    marginBottom: 80, // Adjusted marginBottom to accommodate navbar
+  },
+  servicioContainer: {
     marginBottom: 10,
+    padding: 10,
+    backgroundColor: '#f0f0f0',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#ccc',
   },
   navbar: {
     flexDirection: 'row',
-    justifyContent: 'space-around', // Changed from 'space-between' to 'space-around' to reduce space between buttons
+    justifyContent: 'space-around',
     backgroundColor: '#4A4E69',
-    paddingHorizontal: 10, // Reduced paddingHorizontal to make buttons closer
-    paddingVertical: 20, // Increased padding vertical to make the navbar larger
+    paddingHorizontal: 10,
+    paddingVertical: 20,
     position: 'absolute',
     bottom: 0,
     left: 0,
@@ -275,13 +199,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   icon: {
-    width: 25, // Increased icon size
-    height: 25, // Increased icon size
-    marginBottom: 5, // Reduced marginBottom to make buttons closer
+    width: 25,
+    height: 25,
+    marginBottom: 5,
   },
   navText: {
     color: 'white',
-    fontSize: 14, // Increased font size for better visibility
+    fontSize: 14,
   },
 });
 
